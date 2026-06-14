cce85748c164e67556d78d865e044313ab8ccf71
JDiazD33 <u23232498@utp.edu.pe>
2026-06-13 22:58:56 -0500

    Initial Commit

---
+++ RutaUTP/RutaUTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RutaUTPApp.swift
+//  RutaUTP
+//
+//  Created by Joaquin Diaz on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RutaUTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
